2be2dd3b5e9fd6196e0eb953e619a152dd3acfb6
Ashika-18 <ashibasama@gmail.com>
2023-06-08 06:48:52 +0900

    Initial Commit

---
+++ collectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  collectionView
+//
+//  Created by Kazuhiro Ashiba on 2023/06/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
